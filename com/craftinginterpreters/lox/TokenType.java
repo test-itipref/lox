1563edeb27e01e2b2db7f64d6c1f4da3884e4b78
test-itipref <homatsuka@outlook.jp>
2023-11-15 01:14:17 +0900

    first-commit

---
+++ com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // 記号一個のトークン
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEPERATOR, SLASH, STAR,
+
+    // 記号一個または二個によるトークン
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // リテラル
+    IDENTIFIER, STRING, NUMBER,
+
+    // キーワード
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
