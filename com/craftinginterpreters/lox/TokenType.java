24431a5d2065a51033036fa8dd9c99c14024da15
test-itipref <homatsuka@outlook.jp>
2023-11-15 01:24:30 +0900

    Add-scanning

--- com/craftinginterpreters/lox/TokenType.java
+++ com/craftinginterpreters/lox/TokenType.java
@@ -3,7 +3,7 @@ package com.craftinginterpreters.lox;
 enum TokenType {
     // 記号一個のトークン
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEPERATOR, SLASH, STAR,
+    COMMA, DOT, MINUS, PLUS, SEPERATOR, SLASH, STAR, SEMICOLON,
 
     // 記号一個または二個によるトークン
     BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
